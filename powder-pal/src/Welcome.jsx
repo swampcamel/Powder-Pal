0b5c14e0375fda6834f117809b649f8a8caae1e5
Dan Mace <dmacebeta@gmail.com>
2018-12-01 22:30:51 -0800

    create welcome component

---
+++ powder-pal/src/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Welcome.scss'
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="welcome-wrapper">Welcome</div>
+    )
+  }
+}
+
+export default Welcome;
