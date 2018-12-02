f1fd257c7017451632d324b7a036b7fa63e3036b
Dan Mace <dmacebeta@gmail.com>
2018-12-02 12:09:40 -0800

    add navbar

--- powder-pal/src/components/App.js
+++ powder-pal/src/components/App.js
@@ -7,11 +7,20 @@ import Welcome from './Welcome/Welcome';
 import MainFooter from './MainFooter';
 import SecondaryFooter from './SecondaryFooter';
 
+import {Navbar, NavItem} from 'react-materialize';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="navbar-fixed custom-nav">
+          <Navbar className="transparent" brand='POWDER PAL' right>
+            <NavItem href='components.html'>WHAT IS THIS</NavItem>
+            <NavItem onClick={() => console.log('test click')}>SIGN UP</NavItem>
+            <NavItem href='components.html'>LOG IN</NavItem>
+          </Navbar>
+        </div>
         <Welcome/>
         <MainFooter/>
         <SecondaryFooter/>
--- powder-pal/src/components/Welcome/HeroContent.jsx
+++ powder-pal/src/components/Welcome/HeroContent.jsx
@@ -6,8 +6,8 @@ function HeroContent() {
   return (
     <div className="hero-content">
       <div className="hero-content-blurb">
-        <h2>You only get this chance one season a year.</h2>
-        <h1 >Get out there.</h1>
+        <h3>You only get this chance one season a year.</h3>
+        <h2>Get out there.</h2>
       </div>
       <div className="hero-content-search"><div className="search-icon">Icon</div><div className="search-input"><input placeholder="Enter a location"></input></div></div>
     </div>
