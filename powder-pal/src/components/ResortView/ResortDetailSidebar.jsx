4f1a6c24904a5425573f849421f9fae2287d7a33
Dan Mace <dmacebeta@gmail.com>
2018-12-07 16:54:31 -0800

    improve detail sidebar

--- powder-pal/src/components/ResortView/ResortDetailSidebar.jsx
+++ powder-pal/src/components/ResortView/ResortDetailSidebar.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './ResortDetailSidebar.scss'
+import {Button} from 'react-materialize';
 
 function ResortDetailSidebar() {
   return(
-    <div className='detail-sidebar'>Resort Details</div>
+    <div className='detail-sidebar'>
+      <div className='detail-flex'>
+        <span>--Image--</span>
+        <h4>Resort Name</h4>
+        <h5>Location Line 1</h5>
+        <h5>Location Line 2</h5>
+        <span>*****</span>
+        <span># of Reviews</span>
+        <Button>Add to Favorites</Button>
+      </div>
+    </div>
   );
 }
 
