fd5056fda0a3cdb875420ebde3c4754dd3a7bd61
Dan Mace <dmacebeta@gmail.com>
2018-12-18 20:47:44 -0800

    change over api loop request and filter by distance to action types and bring filteredList into state

--- powder-pal/src/actions/index.js
+++ powder-pal/src/actions/index.js
@@ -94,17 +94,51 @@ export function getResortListSnapshot() {
 //   })}
 // }
 
-export function getLiftieResortData(fullListofShit) {
-  return function (dispatch){
-    if(etc)
-  return fetch(`https://liftie.info/api/resort/${query}`).then(
-    response => response.json(),
-    error => console.log("FAIL", error)
-  ).then(function(resortData) {
-    if(resortData.id) {
-      dispatch(getLiftieResort(resortData));
+export function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+  //default coords for Portland OR (remove lat1 and lon1 from parameters to use)
+  // const lat1 = 45.520780;
+  // const lon1 = -122.677398;
+  if ((lat1 == lat2) && (lon1 == lon2)) {
+    return 0;
+  }
+  else {
+    var radlat1 = Math.PI * lat1/180;
+    var radlat2 = Math.PI * lat2/180;
+    var theta = lon1-lon2;
+    var radtheta = Math.PI * theta/180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
     }
-  })}
+    dist = Math.acos(dist);
+    dist = dist * 180/Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    return Math.round(dist);
+  }
+}
+
+export function getLiftieResortData(fullList, geoCoords) {
+  return function (dispatch){
+    const filteredList = [];
+    fullList.forEach((resortInList, index) => {
+      let distance = calculateDistance(geoCoords.lat, geoCoords.lng, resortInList.ll[1], resortInList.ll[0], "M");
+      if (distance < 1000) {
+        filteredList.push(resortInList);
+      }
+    });
+    filteredList.forEach((resortInFilteredList) => {
+      return fetch(`https://liftie.info/api/resort/${resortInFilteredList.id}`).then(
+        response => response.json(),
+        error => console.log("FAIL", error)
+        ).then(function(resortData) {
+          if(resortData.id) {
+            dispatch(getLiftieResort(resortData));
+        }
+      });
+    });
+  }
 }
 
 export function getUserGeoCode(query) {
--- powder-pal/src/components/SearchResults/SearchResults.jsx
+++ powder-pal/src/components/SearchResults/SearchResults.jsx
@@ -14,43 +14,17 @@ class SearchResults extends Component {
 
   componentWillMount() {
     this.props.dispatch(fetchResorts());
+    this.props.dispatch(getLiftieResortData(this.props.data.liftieData, this.props.data.userGeo));
   }
 
-  calculateDistance(lat1, lon1, lat2, lon2, unit) {
-    //default coords for portland
-    // const lat1 = 45.520780;
-    // const lon1 = -122.677398;
-    if ((lat1 == lat2) && (lon1 == lon2)) {
-      return 0;
-    }
-    else {
-      var radlat1 = Math.PI * lat1/180;
-      var radlat2 = Math.PI * lat2/180;
-      var theta = lon1-lon2;
-      var radtheta = Math.PI * theta/180;
-      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-      if (dist > 1) {
-        dist = 1;
-      }
-      dist = Math.acos(dist);
-      dist = dist * 180/Math.PI;
-      dist = dist * 60 * 1.1515;
-      if (unit=="K") { dist = dist * 1.609344 }
-      if (unit=="N") { dist = dist * 0.8684 }
-      return Math.round(dist);
-    }
-  }
+
   render() {
     return(
       <div className="search-wrapper">
         <SearchSidebar/>
         <div className="results-wrapper">
           <TopFeature/>
-          {this.props.data.liftieData.map((resort, index) => {
-            let distance = this.calculateDistance(this.props.data.userGeo.lat, this.props.data.userGeo.lng, resort.ll[1], resort.ll[0], "M");
-            if( distance < 1000) {
-
-              return (<Resort key={index} id={index} liftieInfo={resort} distance={distance} resorts={this.props.data.resorts} placeCandidates={this.props.data.placeCandidates} placePhotoURL={this.props.data.placePhotoURL}/>);
+          <Resort resorts={this.props.data.resorts} placeCandidates={this.props.data.placeCandidates} placePhotoURL={this.props.data.placePhotoURL}/>);
               }
           })}
 
@@ -70,7 +44,8 @@ const mapStateToProps = state => {
       placeCandidates: state.placeCandidates,
       placePhotoURL: state.placePhotoURL,
       liftieData: state.liftieData,
-      userGeo: state.userGeo
+      userGeo: state.userGeo,
+      filteredResults: state.filteredResults
     };
   } else {
       data = {
